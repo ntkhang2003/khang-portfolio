ae8a4e57777f11103b17d8233eac2cd230433963
ntkhang2003 <ntkhang2003@gmail.com>
2025-12-27 13:05:12 +0700

    fix: update assets path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/khang-portfolio',
+  assetPrefix: '/khang-portfolio',
 };
 
 export default nextConfig;
